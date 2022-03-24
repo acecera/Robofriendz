a7fb8bbfc47d52ed213833fb64aa1bd4d9a53a4d
Anthony Cera <ace_cera@yahoo.com>
2022-03-24 12:52:49 -0700

    added a card component, searchbox component, robot list, and styling to app.

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1, 
+        name: 'Anthony Cera',
+        username: 'Ace',
+        email: 'anthonyanthony@me.com'
+    },
+    {
+        id: 2, 
+        name: 'Giovanni Michael',
+        username: 'Gbreezy',
+        email: 'givoanni@me.com'
+    },
+    {
+        id: 3,
+        name: 'Nicholle Marie',
+        username: 'Bolita98',
+        email: 'nicholle@aol.net'
+    },
+    {
+        id: 4, 
+        name: 'Fisher',
+        username: 'DJFish',
+        email: 'bookings@fisher.com'
+    },
+    {
+        id: 5, 
+        name: 'Daft Punk',
+        username: 'StillHere',
+        email: 'frenchrobots@daftpunkmusic.com'
+    },
+    {
+        id: 6,
+        name: 'Deadmau5',
+        username: '#1somewhere',
+        email: 'mau5trap@bookings.com'
+    },
+    {
+        id: 7,
+        name: 'Patricia Espinoza',
+        username: 'Miss Patty',
+        email: 'patty@yahoo.com'
+    },
+    {
+        id: 8, 
+        name: 'Palmerino Frank',
+        username: 'Dino',
+        email: 'thedino@hotmail.net'
+    },
+    {
+        id: 9, 
+        name: 'Polo',
+        username: 'NotPan',
+        email: 'polo&pan@bookings.net'
+    },
+    {
+        id: 10, 
+        name: 'Pan',
+        username: 'PanNotPolo',
+        email: 'pan4eva@gmail.com'
+    }
+]
