5504cd3571456e49d00dc72712ddf76fe38fc3ca
Anthony Cera <ace_cera@yahoo.com>
2022-03-24 16:31:36 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import CardList from './CardList';
+import CardList from './components/CardList';
 import { robots } from './robots';
 
 
