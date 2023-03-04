1b711b5f25bb6d907300103d54de2778a92cd6a0
Anthony Cera <ace_cera@yahoo.com>
2023-03-03 16:49:47 -0800

    fixed spacing on line of code to model best practices.

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -21,7 +21,7 @@ class CounterButton extends Component {
     render() {
         return (
             <button color={this.props.color} onClick={this.updateCount}>
-            Count: {this.state.count}
+                Count: {this.state.count}
             </button>
         )
     }
